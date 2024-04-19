9f19bd710c42687c1c61aa80cb7f3885ad3880f5
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2024-04-20 00:59:32 +0100

    fetch the two apis for display 4 famous coins and also the markets

--- app/Http/Controllers/MarketController.php
+++ app/Http/Controllers/MarketController.php
@@ -9,9 +9,12 @@ class MarketController extends Controller
 {
     function index()
     {
+        $response = Http::get('https://api.coincap.io/v2/assets');
+        $cryptos = $response->json()['data'];
+
         $markets = Http::get('api.coincap.io/v2/markets')->json();
         $marketsCollection = collect($markets['data']);
         $marketsData = $markets['data'];
-        return view('homePages.market', compact('marketsData'));
+        return view('homePages.market', compact('marketsData','cryptos'));
     }
 }
